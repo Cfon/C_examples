74faee5dab87e356b14006e6e47a4f5b5b3e2fa0
Cfon <kimalekkz@gmail.com>
2021-10-30 10:55:23 +0500

    updated

--- pointers_3.c
+++ pointers_3.c
@@ -8,37 +8,29 @@
 int main()
 { 
   SetConsoleOutputCP(CP_UTF8);
-  // setlocale(LC_ALL, "russian"); 
+  // setlocale(LC_ALL, "russian");   
   
   int x[] = { 0, 2, 10, 99 };
   int *i = x;
   int y;
   
   y = *i;
-  printf("%d, %p\n", y, i); // y == 0, i == &x[0]
+  printf("%d, %p\n", y, i); 
   
-  y = *i++;  // y = *i, i++
-  printf("%d, %p\n", y, i); // y == 0, i == &x[1]
+  y = *i++;  
+  printf("%d, %p\n", y, i); 
   
-  y = ++*i;  // y = ++(*i)
-  printf("%d, %p\n", y, i); // y == 3, i == &x[1]
+  y = ++*i;  
+  printf("%d, %p\n", y, i); 
    
-  y = *++i;  // y = *(++i) 
-  printf("%d, %p\n", y, i); // y == 10, i == &x[2]
+  y = *++i;  
+  printf("%d, %p\n", y, i); 
   
-  y = (*i)++; // #1, #2
-  printf("%d, %p\n", y, i); // y == 10, i == &x[2]
+  y = (*i)++; 
+  printf("%d, %p\n", y, i); 
   
-  y = ++(*i); // почему y == 12? см. #2
-  printf("%d, %p\n", y, i); // y == 12, i == &x[2]
-  
-  // #1
-  // int a = 10, y;
-  // y = a++;
-  // printf("%d, %d\n", a, y);
-  
-  // #2
-  // y = (*i)++; // изменение значения в самом массиве x
-  // printf("%d, %p, %d\n", y, i, x[2]); // y == 10, i == &x[2], x[2] == 11
+  y = ++(*i); 
+  printf("%d, %p\n", y, i); 
+   
   
 }
